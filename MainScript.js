98867fe8606be3786af142a37bd75280542c1c65
Ritin Malhotra <rm5486@nyu.edu>
2022-08-26 03:15:31 +0530

    Add Color Changing Functionality

--- MainScript.js
+++ MainScript.js
@@ -10,3 +10,21 @@ shareButtons.forEach(button => button.addEventListener('click', () => {
         button.style.color = null;
     }, 500);
 }))
+
+let colorChangerText = document.querySelector('#color-changer');
+colorChangerText.addEventListener('click', () => {
+    let sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('light');
+
+    let topContentBox = document.querySelector('.top-intro');
+    topContentBox.classList.toggle('light');
+
+    let topRightBox = document.querySelector('.top-side-content');
+    topRightBox.classList.toggle('light');
+
+    let bottomRightBox = document.querySelector('.bottom-side-content');
+    bottomRightBox.classList.toggle('light');
+
+    let footer = document.querySelector('.footer');
+    footer.classList.toggle('light');
+})
